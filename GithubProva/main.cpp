58da9dff96237b6f269ab85a4ff480f3a2766011
Luca Caruso <krogan0121@gmail.com>
2022-03-03 15:33:56 +0100

    Initial Commit

---
+++ GithubProva/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GithubProva
+//
+//  Created by Luca Caruso on 03/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
